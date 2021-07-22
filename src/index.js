ce61d068580bbdbb963c21fbdf0541935e4fe568
Vitaliy <vtk0679@gmail.com>
2021-07-23 00:29:43 +0300

    adds js and templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './sass/main.scss';
+
+import menuTemplate from './templates/menu.hbs';
+import menuData from './menu.json';
+
+const STORAGE_KEY = 'isDarkTheme';
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitchRef = document.getElementById('theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+const menuRef = document.querySelector('ul.js-menu');
+
+themeSwitchRef.onchange = onThemeChange;
+
+onPageLoad();
+
+function onThemeChange() {
+  bodyRef.classList.toggle(Theme.LIGHT);
+  bodyRef.classList.toggle(Theme.DARK);
+  if (bodyRef.classList.contains(Theme.DARK)) localStorage.setItem(STORAGE_KEY, true);
+  else localStorage.setItem(STORAGE_KEY, false);
+}
+
+function onPageLoad() {
+  if (localStorage.getItem(STORAGE_KEY) === 'true') {
+    themeSwitchRef.checked = true;
+    bodyRef.classList.add(Theme.DARK);
+  } else bodyRef.classList.add(Theme.LIGHT);
+
+  renderMenu();
+}
+
+function renderMenu() {
+  const markup = menuTemplate(menuData);
+  menuRef.innerHTML = markup;
+}
